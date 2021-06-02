0432d62a8f3308861315583e673bd532f597beb7
Kyle <kaledoux@gmail.com>
2021-06-02 09:29:45 -0400

    uuid module added

--- queries.js
+++ queries.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('uuid');
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -9,65 +10,97 @@ const pool = new Pool({
 	port     : 5432
 });
 
-const getUsers = (request, response) => {
-	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+// const getUsers = (request, response) => {
+// 	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		response.status(200).json(results.rows);
+// 	});
+// };
+
+// const getUserById = (request, response) => {
+// 	const id = parseInt(request.params.id);
+
+// 	pool.query('SELECT * FROM users WHERE id = $1', [ id ], (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		response.status(200).json(results.rows);
+// 	});
+// };
+
+// const createUser = (request, response) => {
+// 	const { name, email } = request.body;
+
+// 	pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [ name, email ], (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		console.log(results);
+// 		response.status(201).send('User added!');
+// 	});
+// };
+
+// const updateUser = (request, response) => {
+// 	const id = parseInt(request.params.id);
+// 	const { name, email } = request.body;
+
+// 	pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [ name, email, id ], (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		response.status(200).send(`User modified with ID: ${id}`);
+// 	});
+// };
+
+// const deleteUser = (request, response) => {
+// 	const id = parseInt(request.params.id);
+
+// 	pool.query('DELETE FROM users WHERE id = $1', [ id ], (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		response.status(200).send(`User deleted with ID: ${id}`);
+// 	});
+// };
+
+const getRequests = (request, response) => {
+	const id = request.params.uuid;
+
+	pool.query('SELECT request FROM requests WHERE bin_id = $1', [ id ], (error, results) => {
 		if (error) {
 			throw error;
 		}
-		response.status(200).json(results.rows);
+		response.status(200).render('bin', { uuid: id, requests: results.rows });
 	});
 };
 
-const getUserById = (request, response) => {
-	const id = parseInt(request.params.id);
-
-	pool.query('SELECT * FROM users WHERE id = $1', [ id ], (error, results) => {
+const createEndpoint = (request, response) => {
+	const identifier = uuidv4();
+	pool.query('INSERT INTO bins (uuid) VALUES ($1)', [ identifier ], (error, results) => {
 		if (error) {
 			throw error;
 		}
-		response.status(200).json(results.rows);
+		response.status(201);
 	});
+	return identifier;
 };
 
-const createUser = (request, response) => {
-	const { name, email } = request.body;
+const createRequest = (request, response) => {
+	const id = request.params.uuid;
+	const body = request.body;
 
-	pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [ name, email ], (error, results) => {
+	pool.query('INSERT INTO requests (bin_id, request) VALUES ($1, $2)', [ id, body ], (error, results) => {
 		if (error) {
 			throw error;
 		}
-		console.log(results);
-		response.status(201).send('User added!');
-	});
-};
-
-const updateUser = (request, response) => {
-	const id = parseInt(request.params.id);
-	const { name, email } = request.body;
-
-	pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [ name, email, id ], (error, results) => {
-		if (error) {
-			throw error;
-		}
-		response.status(200).send(`User modified with ID: ${id}`);
-	});
-};
-
-const deleteUser = (request, response) => {
-	const id = parseInt(request.params.id);
-
-	pool.query('DELETE FROM users WHERE id = $1', [ id ], (error, results) => {
-		if (error) {
-			throw error;
-		}
-		response.status(200).send(`User deleted with ID: ${id}`);
+		response.status(201).send(`Request added into bin with UUID ${id}`);
 	});
 };
 
 module.exports = {
-	getUsers,
-	getUserById,
-	createUser,
-	updateUser,
-	deleteUser
+	getRequests,
+	createEndpoint,
+	createRequest
 };
