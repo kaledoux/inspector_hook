c931560c289141b1d7386855b2eb0625a095a2f7
Kyle <kaledoux@gmail.com>
2021-06-03 11:18:14 -0400

    refactor queries for JSON output

--- queries.js
+++ queries.js
@@ -10,61 +10,6 @@ const pool = new Pool({
 	port     : 5432
 });
 
-// const getUsers = (request, response) => {
-// 	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-// 		if (error) {
-// 			throw error;
-// 		}
-// 		response.status(200).json(results.rows);
-// 	});
-// };
-
-// const getUserById = (request, response) => {
-// 	const id = parseInt(request.params.id);
-
-// 	pool.query('SELECT * FROM users WHERE id = $1', [ id ], (error, results) => {
-// 		if (error) {
-// 			throw error;
-// 		}
-// 		response.status(200).json(results.rows);
-// 	});
-// };
-
-// const createUser = (request, response) => {
-// 	const { name, email } = request.body;
-
-// 	pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [ name, email ], (error, results) => {
-// 		if (error) {
-// 			throw error;
-// 		}
-// 		console.log(results);
-// 		response.status(201).send('User added!');
-// 	});
-// };
-
-// const updateUser = (request, response) => {
-// 	const id = parseInt(request.params.id);
-// 	const { name, email } = request.body;
-
-// 	pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [ name, email, id ], (error, results) => {
-// 		if (error) {
-// 			throw error;
-// 		}
-// 		response.status(200).send(`User modified with ID: ${id}`);
-// 	});
-// };
-
-// const deleteUser = (request, response) => {
-// 	const id = parseInt(request.params.id);
-
-// 	pool.query('DELETE FROM users WHERE id = $1', [ id ], (error, results) => {
-// 		if (error) {
-// 			throw error;
-// 		}
-// 		response.status(200).send(`User deleted with ID: ${id}`);
-// 	});
-// };
-
 const getRequests = (request, response) => {
 	const id = request.params.uuid;
 
@@ -76,7 +21,7 @@ const getRequests = (request, response) => {
 		if (error) {
 			throw error;
 		}
-		response.status(200).send({ uuid: id, requests: results.rows });
+		response.status(200).json({ uuid: id, requests: results.rows });
 	});
 };
 
@@ -87,7 +32,6 @@ const createEndpoint = (request, response) => {
 			throw error;
 		}
 		response.status(201).json({ binID: identifier });
-		// response.status(201).send(`New bin created: ${identifier}`);
 	});
 	return identifier;
 };
